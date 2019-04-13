01097d8c7f6bcdcbd6663b32fe05b389514a56db
sa3d clay <sa3d.clay@gmail.com>
2019-04-13 20:22:20 +0200

    merge

--- src/project3/scanner.java
+++ src/project3/scanner.java
@@ -11,8 +11,69 @@ package project3;
  */
 //No built_in methods
 public class scanner {
-    private static String text;
-    public scanner(String text){
-        scanner.text=text;
+    private static char[] str;
+    public static int index;
+    
+    public scanner(char[] str){
+        scanner.setStr(str);
     }
+    
+    public static char[] getStr() {
+		return str;
+	}
+
+	public static void setStr(char[] str) {
+		scanner.str = str;
+	}
+
+	
+	
+//	public boolean matchStart(char[] str, int index) {
+//		switch( str[index] ) {
+//		case '@':
+//		case '^':
+//			if( matchClassDeclaration(str, index++) )
+//				if( matchEnd(str) )
+//					return true;
+//			break;
+//		default:
+//			return false;
+//		}
+//		return false;
+//	}
+//	
+//	public boolean matchClassDeclaration(char[] str, int index) {
+//		switch( str[index] ) {
+//		case 'T':
+//			if(str[index++] == 'y')
+//				if(str[index++] == 'p')
+//					if(str[index++] == 'e')
+//						if( matchID(str, index) )
+//			break;
+//		default:
+//			return false;
+//		}
+//		return false;
+//	}
+//	
+//	public boolean matchID(char[] str, int index) {
+//		if(str[index] >= 65 && str[index] <=122 ) {
+//			while(str[index] != ' ') {
+//				index++;
+//			}
+//			return true;
+//		}
+//		return false;
+//	}
+//	
+//	public boolean matchEnd(char[] str) {
+//		switch(str[-1]) { // -1 will be replaced with the index
+//		case '$':
+//		case '#':
+//			return true;
+//		default:
+//			return false;
+//		}
+//	}
+
 }
